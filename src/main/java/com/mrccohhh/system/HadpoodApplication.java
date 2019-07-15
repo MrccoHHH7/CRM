6515a78566f20d818f1af3722e9d38c3c6f59e24
王硕 <@DESKTOP-S4R5I8H>
2019-07-15 11:23:23 +0800

    test

---
+++ src/main/java/com/mrccohhh/system/HadpoodApplication.java
@@ -0,0 +1,13 @@
+package com.mrccohhh.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadpoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadpoodApplication.class, args);
+	}
+
+}
